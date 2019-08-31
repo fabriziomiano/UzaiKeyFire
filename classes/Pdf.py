9acc75e2c4e8964700e6dc24af3f1ba2b9ff8290
Fabrizio Miano <fabriziomiano@gmail.com>
2019-08-31 23:26:32 +0200

    Add classes

---
+++ classes/Pdf.py
@@ -0,0 +1,57 @@
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+class PdfHandler(object):
+    """
+    Class to convert the content of a PDF into a string.
+
+    Attributes:
+        stream_in (bytes): byte-like content of PDF
+
+    """
+
+    def __init__(self, stream_in):
+        self.stream_in = stream_in
+
+    def get_corpus(self):
+        """
+        Return a string made of the content of the PDF
+
+        """
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(
+            rsrcmgr, retstr, codec=codec, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        maxpages = 0
+        caching = True
+        pagenos = set()
+        pages = PDFPage.get_pages(
+            self.stream_in,
+            pagenos,
+            maxpages=maxpages,
+            password="",
+            caching=caching,
+            check_extractable=True
+        )
+        for page in pages:
+            interpreter.process_page(page)
+        device.close()
+        text = retstr.getvalue()
+        retstr.close()
+        return text
+
+    def save_to_txt(self):
+        """
+        Save a .txt file with the content of the PDF
+        """
+        content = self.get_corpus()
+        txt_pdf = open('text_pdf.txt', 'wb')
+        txt_pdf.write(content.encode('utf-8'))
+        txt_pdf.close()
---
+++ classes/Text.py
@@ -0,0 +1,80 @@
+import unicodedata
+import string
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+STOPWORDS = set(stopwords.words('english'))
+STEMMER = SnowballStemmer("english")
+
+
+class TextPreprocessor(object):
+    def __init__(self, text):
+        self.text = text
+
+    def remove_non_ascii(self):
+        """
+        Remove non-ASCII charachters from
+        a given string
+
+        :return: str
+        """
+        onlyascii_tokens = []
+        for token in self.text.split():
+            token = unicodedata.normalize('NFKD', token)\
+                .encode('ascii', 'ignore').decode('utf-8', 'ignore')
+            onlyascii_tokens.append(token)
+        self.text = " ".join(token for token in onlyascii_tokens)
+        return self.text
+
+    def remove_stopwords(self):
+        """
+        Remove stopwords from a given string
+
+        :return: str
+        """
+        tokens = [t for t in self.text.split()
+                  if t not in STOPWORDS]
+        self.text = " ".join(t for t in tokens)
+
+    def remove_punctuation(self):
+        """
+        Remove punctuation from a given string
+
+        :return: str
+        """
+        tokens = [
+            token.translate(str.maketrans("", "", string.punctuation))
+            for token in self.text.split()
+        ]
+        self.text = " ".join(t for t in tokens)
+
+    def stem_text(self):
+        """
+        Return a stemmed string
+
+        :return: list of stemmed tokens
+        """
+        tokens = self.text.split()
+        self.text = " ".join(STEMMER.stem(t) for t in tokens)
+
+    def base_preprocess(self):
+        """
+        Perform standard NLP preprocessing:
+        Tokenization, non-ASCII chars and stopwords removal
+
+        :return: self.text
+        """
+        self.text = self.text.lower()
+        self.remove_non_ascii()
+        self.remove_stopwords()
+        self.remove_punctuation()
+        return self.text
+
+    def preprocess(self):
+        """
+        Perform base_preprocess() and stemming
+
+        :return: self.text
+        """
+        self.base_preprocess()
+        self.stem_text()
+        return self.text
