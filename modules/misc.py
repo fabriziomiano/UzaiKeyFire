505c0a98a0f178f4c4e18e8a23ff3c327c703de5
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-01 04:35:55 +0200

    Move from utils to modules.plot or modules.data

--- modules/misc.py
+++ modules/misc.py
@@ -1,10 +1,7 @@
 import os
 import errno
 import logging
-from collections import Counter
 from classes.Pdf import PdfHandler
-from matplotlib import pyplot as plt
-import seaborn as sns
 
 
 def get_logger(name):
@@ -53,30 +50,6 @@ def extract_text(stream_in):
     return corpus
 
 
-def save_barplot(data, labels, n_max, path, title):
-    """
-    Save bar plot of given data in format list(tuples)
-
-    :param data: list of tuples
-    :param labels: list: x and y axis labels in this order
-    :param n_max: int: max number of elements
-    :param path: str: output file path
-    :param title: str
-    :return: None
-    """
-    x, y = zip(*data)
-    plt.figure(figsize=(n_max, 10))
-    ax = sns.barplot(
-        list(x)[:n_max],
-        list(y)[:n_max],
-        palette="Blues_d")
-    ax.set_title(title)
-    plt.xticks(rotation=30)
-    plt.xlabel(labels[0])
-    plt.ylabel(labels[1], labelpad=60, rotation=90)
-    plt.savefig(path)
-
-
 def get_project_name(file_path):
     """
     Given a file path return a string
@@ -108,42 +81,3 @@ def create_nonexistent_dir(path, exc_raise=False):
                 "Could not create directory with path: {}".format(path))
             if exc_raise:
                 raise
-
-
-def kwords_count(corpus):
-    """
-    Perfom word count on a given corpus
-    Return a descedant sorted list of tuples(word, count)
-    :param corpus: list
-    :return: list
-    """
-    return Counter(corpus.split()).most_common()
-
-
-def get_adverbs(doc):
-    """
-    Given a spacy-parsed document
-    return a list with all the adverbs in it.
-    :param doc: spacy.tokens.doc.Doc parsed document
-    :return: list
-    """
-    return [
-        token.lower_ for sent in doc.sents
-        for token in sent if token.pos_ == "ADV"
-    ]
-
-
-def adv_stats(adverbs):
-    """
-    Return a dict with the % of occurrence of the adverbs
-    in a given non-empty list of adverbs
-    :param adverbs: list of adverbs
-    :return:
-    """
-    adv_count = Counter(adverbs).most_common()
-    adv_data = [
-        (el[0], el[1] / len(adv_count))
-        for el in adv_count
-    ]
-    return adv_data
-
