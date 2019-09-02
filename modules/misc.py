f6ae4f022cd262bb919bc49c4811839f334d5908
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-02 22:27:54 +0200

    nuances

--- modules/misc.py
+++ modules/misc.py
@@ -4,24 +4,24 @@ import logging
 from wordcloud import WordCloud
 from classes.Pdf import PdfHandler
 ENTITY_ABBRV_MAP = {
-  "PERSON": "People, including fictional",
-  "NORP": "Nationalities or religious or political groups",
-  "FAC": "Buildings, airports, highways, bridges, etc",
-  "ORG": "Companies, agencies, institutions, etc",
-  "GPE": "Countries, cities, states",
-  "LOC": "Non-GPE locations, mountain ranges, bodies of water",
-  "PRODUCT": "Objects, vehicles, foods, etc. (Not services.",
-  "EVENT": "Named hurricanes, battles, wars, sports events, etc",
-  "WORK_OF_ART": "Titles of books, songs, etc",
-  "LAW": "Named documents made into laws",
-  "LANGUAGE": "Any named language",
-  "DATE": "Absolute or relative dates or periods",
-  "TIME": "Times smaller than a day",
-  "PERCENT": "Percentage, including ”%“",
-  "MONEY": "Monetary values, including unit",
-  "QUANTITY": "Measurements, as of weight or distance",
-  "ORDINAL": "'first', 'second', etc",
-  "CARDINAL": "Numerals that do not fall under another type"
+    "CARDINAL": "Numerals not under another type",
+    "DATE": "Absolute or relative dates",
+    "EVENT": "Hurricanes, wars, sports events,...",
+    "FAC": "Facilities",
+    "GPE": "Countries, cities, states",
+    "LANGUAGE": "Any named language",
+    "LAW": "Named documents made into laws",
+    "LOC": "Non-GPE locations",
+    "MONEY": "Monetary values, including unit",
+    "NORP": "Nationalities, religious, or political groups",
+    "ORDINAL": "”first“, ”second“,...",
+    "ORG": "Companies, agencies, institutions",
+    "PERCENT": "Percentage, including ”%“",
+    "PERSON": "People, including fictional",
+    "PRODUCT": "Objects, vehicles, foods, etc.",
+    "QUANTITY": "Measurements",
+    "TIME": "Times smaller than a day",
+    "WORK_OF_ART": "Titles of books, songs,...",
 }
 
 
